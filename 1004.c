a09469cfb29cbfd6fb5538563c643e4e5a0a9bbf
Gustavo Neves <gustavohenrique19022@gmail.com>
2025-04-07 15:37:21 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    #include <stdio.h>
+ 
+int main() {
+ 
+    int A,B;
+    
+    scanf("%d %d", &A, &B);
+    
+    int PROD = A*B;
+    
+    printf("PROD = %d\n",PROD);
+    
+ 
+    return 0;
+
+ 
